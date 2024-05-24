c32b4333929b8dd7745570f80ec0a9ad31245a99
nikiosti <yaostin@proton.me>
2024-05-25 02:38:50 +0300

    change image url

--- constants/api-constants.ts
+++ constants/api-constants.ts
@@ -2,4 +2,4 @@ export const CLIENT_MOVIES_URL = 'https://ss-2024-proxy.onrender.com/api/discove
 export const CLIENT_GENRE_URL = 'https://ss-2024-proxy.onrender.com/api/genre/movie/list'
 export const CLIENT_MOVIE_DETAIL_URL = 'https://ss-2024-proxy.onrender.com/api/movie'
 
-export const TMDB_IMAGE_URL = 'https://image.tmdb.org/t/p/original'
+export const TMDB_IMAGE_URL = 'https://image.tmdb.org/t/p/w500'
