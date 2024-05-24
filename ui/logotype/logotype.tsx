1483883ba276ff5d36d9caee5277f34cf9b70a6e
nikiosti <yaostin@proton.me>
2024-05-24 23:12:01 +0300

    change : logo

--- ui/logotype/logotype.tsx
+++ ui/logotype/logotype.tsx
@@ -1,13 +1,18 @@
-import { Group, Title } from '@mantine/core'
+'use client'
+import { Group, Title, UnstyledButton } from '@mantine/core'
 import Image from 'next/image'
+import { useRouter } from 'next/navigation'
 
 export const LogotypeWithTitle = () => {
+  const router = useRouter()
   return (
-    <Group h={36} gap={12}>
-      <Image src="/logotype-small.svg" width={32} height={32} alt="ArrowFlicks logotype" />
-      <Title fw={600} fz={24} c="purple.5" ff="Poppins, sans-serif" fs="normal" lts={-0.48} lh="normal">
-        ArrowFlicks
-      </Title>
-    </Group>
+    <UnstyledButton onClick={() => router.push('/')}>
+      <Group h={36} gap={12}>
+        <Image src="/logotype-small.svg" width={32} height={32} alt="ArrowFlicks logotype" />
+        <Title fw={600} fz={24} c="purple.5" ff="Poppins, sans-serif" fs="normal" lts={-0.48} lh="normal">
+          ArrowFlicks
+        </Title>
+      </Group>
+    </UnstyledButton>
   )
 }
