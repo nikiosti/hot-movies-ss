b3c3513f4d69bc2760eb5dea4bd1711a165effb9
nikiosti <yaostin@proton.me>
2024-05-24 23:36:32 +0300

    change RatedMovieNotFound

--- app/(pages)/rated/_components/rated-movie-not-found/index.tsx
+++ app/(pages)/rated/_components/rated-movie-not-found/index.tsx
@@ -1,16 +1,16 @@
 'use client'
 
 import { Box, Button, Center, Image, Title } from '@mantine/core'
-import { useRouter } from 'next/router'
+import { useRouter } from 'next/navigation'
 
 export const RatedMovieNotFound = () => {
   const router = useRouter()
   return (
     <>
       <Center h="100%">
-        <Box w={468}>
+        <Box>
           <Center>
-            <Image src="/empty-rated.png" w={233} alt="notfound image" />
+            <Image src="/empty-rated.png" w={311} alt="notfound image" />
           </Center>
           <Title ta="center" lh="normal" fw={600} fz={20} fs="normal" order={3} mt={16}>
             You haven&apos;t rated any films yet
