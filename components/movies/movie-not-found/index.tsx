6fed11b5220933721bf4ef76cc04ec0e3598916f
nikiosti <yaostin@proton.me>
2024-05-24 22:14:42 +0300

    change movie not found

--- components/movies/movie-not-found/index.tsx
+++ components/movies/movie-not-found/index.tsx
@@ -7,10 +7,10 @@ export const MovieNotFound = () => {
     <Center>
       <Box w={468}>
         <Center>
-          <Image src="/notfound.png" w={233} alt='notfound image' />
+          <Image src="/notfound.png" w={233} alt="notfound image" />
         </Center>
         <Title ta="center" lh="normal" fw={600} fz={20} fs="normal" order={3}>
-        We don&apos;t have such movies, look for another one
+          We don&apos;t have such movies, look for another one
         </Title>
       </Box>
     </Center>
